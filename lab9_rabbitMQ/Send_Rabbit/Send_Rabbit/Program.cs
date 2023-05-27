9dab1e8676cbff4aa83d2894cd27bc4ca2a14498
Aleksandra <aleksandra.joanna.wolska@gmail.com>
2023-05-27 16:49:47 +0200

    connection to rabbit MQ

---
+++ lab9_rabbitMQ/Send_Rabbit/Send_Rabbit/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+
+    channel.QueueDeclare(queue: "hello-world",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+    var message = "Hello World!";
+    var body = Encoding.UTF8.GetBytes(message);
+    Console.WriteLine("You entered: " + message);
+    channel.BasicPublish(exchange: string.Empty,
+                 routingKey: "hello",
+                 basicProperties: null,
+                 body: body);
+    Console.WriteLine($" [x] Sent {message}");
+
+
+    //string userInput = "";
+    while (true)
+    {
+        Console.Write("Wprowadź wiadomość: ");
+        message = Console.ReadLine();
+        var bodyLoop = Encoding.UTF8.GetBytes(message);
+
+        if (message.ToLower() == "x")
+        {
+            break;
+        }
+
+        Console.WriteLine("Twoja wiadomość: " + message);
+        channel.BasicPublish(exchange: string.Empty,
+                     routingKey: "hello",
+                     basicProperties: null,
+                     body: bodyLoop);
+        Console.WriteLine($" [x] Wysłano {message}");
+    }
+
+
+
+
+}
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
